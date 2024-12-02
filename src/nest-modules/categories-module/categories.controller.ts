f02a5ace09349cd25ca6b76acf8d48a35690c93c
Felipe Medeiros <felipe.meddeiros@hotmail.com>
2024-12-02 20:35:00 -0300

    implement create controlller

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -1,18 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, Inject} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import {CategorySequelizeRepository} from "@core/category/infra/db/sequelize/category-sequelize.repository";
+import {CreateCategoryUseCase} from "@core/category/application/use-cases/create-category/create-category.use-case";
+import {UpdateCategoryUseCase} from "@core/category/application/use-cases/update-category/update-category.use-case";
+import {DeleteCategoryUseCase} from "@core/category/application/use-cases/delete-category/delete-category.use-case";
+import {GetCategoryUseCase} from "@core/category/application/use-cases/get-category/get-category.use-case";
+import {ListCategoriesUseCase} from "@core/category/application/use-cases/list-categories/list-categories.use-case";
+import {CategoryPresenter} from "./categories.presenter";
+import {CategoryOutput} from "@core/category/application/use-cases/common/category-output";
 
 @Controller('categories-module')
 export class CategoriesController {
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase;
 
-  constructor(private categoryRepo: CategorySequelizeRepository) {
-    // console.log(this.categoryRepo);
-  }
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase;
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase;
+
+  @Inject(ListCategoriesUseCase)
+  private listUseCase: ListCategoriesUseCase;
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    const output = await this.createUseCase.execute(createCategoryDto);
+    return CategoriesController.serialize(output);
   }
 
   @Get()
@@ -33,5 +50,9 @@ export class CategoriesController {
   @Delete(':id')
   remove(@Param('id') id: string) {
 
+  }
+
+  static serialize(output: CategoryOutput) {
+    return new CategoryPresenter(output);
   }
 }
